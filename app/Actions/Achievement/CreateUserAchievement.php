56d6399ff874ffd332848043edf5214c6226ac86
Muhammad M <muhammadmp1997@gmail.com>
2023-09-10 20:50:43 +0330

    feat: using a transaction for creating user achievements

--- app/Actions/Achievement/CreateUserAchievement.php
+++ app/Actions/Achievement/CreateUserAchievement.php
@@ -3,18 +3,21 @@
 namespace App\Actions\Achievement;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class CreateUserAchievement
 {
     public function execute(User $user, array $achievement)
     {
-        $user
+        DB::transaction(function () use ($user, $achievement) {
+            $user
             ->achievements()
             ->create([
                 'achievement_id' => $achievement['id'],
             ]);
         
-        $user->score += $achievement['score'];
-        $user->save();
+            $user->score += $achievement['score'];
+            $user->save();
+        });
     }
 }
